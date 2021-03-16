cdd84b836f9fa6cc590b70fd8814b7d20704f8c9
Dorothie <dorothie.slingsby@email.kpu.ca>
2021-03-16 00:15:16 +0000

    I hate you.

--- app.js
+++ app.js
@@ -2,20 +2,29 @@
 // Languages / Dialects Used: NodeJS, MongoDB NoSQL, EJS, JQuery, JavaScript, HTML, CSS, Sass, Bootstrap.
 
 // base setup
-var express    = require("express"), 
-	app          = express(), 
-	Order        = require("./models/orders"),
-	Inventory    = require("./models/inventory"),
-	Accounts     = require("./models/accounts"), 
-	bodyParser   = require("body-parser");
-const mongoose = require('mongoose'); //This is Mongo, someone else can make it work properly.
+var express      = require("express"), 
+	app            = express(), 
+	Order          = require("./models/orders"),
+	Inventory      = require("./models/inventory"),
+	Accounts       = require("./models/accounts"), 
+	bodyParser     = require("body-parser"),
+  localPass      = require("passport-local"), 
+  passLocalMongo = require("passport-local-mongoose"),
+  user           = require("./models/user"),
+  port           = process.env.PORT || 2000;
+const mongoose = require('mongoose');
 
 	// Connect to MongoDB
 	dbURI = 'mongodb+srv://administrator:test1234@info-2413.md3gl.mongodb.net/TestDB?retryWrites=true&w=majority' //user: administrator pw: test 1234 database: TestDB
 	mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then((result) => console.log('Connected to database.'))
-	.then((result) => app.listen(5000)) // This might be redundant as it's mentioned on line 71 (other app.listen in app.js)? [No it's not, line 71 makes the server able to actually exist on a webpage. -D]
+	.then((result) => app.listen(5000))
 	.catch((err) => console.log(err));
+
+	// Passport login setup.
+	passport.use(new localPass(user.authenticate())); 
+	passport.serializeUser(user.serializeUser()); 
+	passport.deserializeUser(user.deserializeUser());
 
 	// mongoose / mongo sandbox routes
 	// Testing implementation, if you add '/add-inventory' for the url, it should pull up this information
@@ -36,9 +45,16 @@ const mongoose = require('mongoose'); //This is Mongo, someone else can make it 
 		})
 	})
 
-app.get("public/assets/cmstyles.css", function(req, res){ //custom css
-	res.render("public/assets/cmstyles.css");
-});
+	//Custom CSS, don't touch.
+	app.get("public/assets/cmstyles.css", function(req, res){
+		res.render("public/assets/cmstyles.css");
+	});
+
+  
+	function isLoggedIn(req, res, next) { 
+			if (req.isAuthenticated()) return next(); 
+			res.redirect("/login"); 
+	}
 
 
 //main functions
@@ -46,13 +62,16 @@ app.get("/", function(req,res){ //index page
 			res.render("index.ejs");
 });
 
+
 app.get("/account", function(req,res){ // account details page
 	res.render("account.ejs");
 });
 
+
 app.get("/asearch", function(req,res){ //advanced search page
 	res.render("asearch.ejs");
 });
+
 
 app.post("/cart", function(req,res){ //submit order
 	var itemList = req.body.itemList;
@@ -67,34 +86,73 @@ app.post("/cart", function(req,res){ //submit order
 	res.render("cart.ejs");
 });
 
+
 app.get("/ibrowse", function(req,res){ // browse items page
 	res.render("ibrowse.ejs");
 });
 
+
 app.get("/login", function(req,res){ //login page
 	res.render("login.ejs");
 });
+app.post("/login", passport.authenticate("local", { 
+    successRedirect: "/secret", 
+    failRedirect: "/login"
+}), function (req, res) { 
+  res.render("login");
+});    
+app.get("/logout", function (req, res) { 
+    req.logout(); 
+    res.redirect("/"); 
+}); 
+
 
 app.get("/obrowse", function(req,res){ // browse / current orders page
 	res.render("obrowse.ejs");
 });
 
+
 app.get("/overview", function(req,res){ // view order page
 	res.render("overview.ejs");
 });
 
+
 app.get("/register", function(req,res){ //register page
 	res.render("register.ejs");
-});
+});  
+app.post("/register", function (req, res) { 
+var username = req.body.username 
+var password = req.body.password 
+user.register(new user({ username: username }), 
+           password, function (err, user) { 
+       if (err) { 
+           console.log(err); 
+           return res.render("register"); 
+        } 
+  
+        passport.authenticate("local")( 
+            req, res, function () { 
+            res.render("secret"); 
+        }); 
+    }); 
+}); 
+
 
 app.get("/search", function(req,res){ //search page
 	res.render("search.ejs");
 });
 
+
 app.get("/tos", function(req,res){ //terms of service page
 	res.render("tos.ejs");
 });
 
+
 app.listen(3000, '0.0.0.0', function(){ //Server start, so is accessible from a link.
 	console.log("Server Start!");
-})
+}) 
+  
+
+app.listen(port, function () { 
+    console.log("Booted up."); 
+}); 
